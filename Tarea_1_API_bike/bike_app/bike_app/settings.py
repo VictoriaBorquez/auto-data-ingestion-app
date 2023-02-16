547dd3a0fdbc6525c4b3e679cd8c7fc5e5998ebf
VictoriaBorquez <victoria.borquez@usach.cl>
2023-02-16 11:06:47 -0300

    modify settings DB postgresql

--- Tarea_1_API_bike/bike_app/bike_app/settings.py
+++ Tarea_1_API_bike/bike_app/bike_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bike.apps.BikeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'bike_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
