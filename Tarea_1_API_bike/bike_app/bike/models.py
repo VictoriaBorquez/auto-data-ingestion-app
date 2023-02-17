28ed8f431780d7f967e9a0d63c0b4f666b667bad
VictoriaBorquez <victoria.borquez@usach.cl>
2023-02-16 21:54:34 -0300

    get and show data

--- Tarea_1_API_bike/bike_app/bike/models.py
+++ Tarea_1_API_bike/bike_app/bike/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
+
+class Location(models.Model):
+    city = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    latitude = models.CharField(max_length=200)
+    longitude = models.CharField(max_length=200)
+
 class Extra(models.Model):
     address = models.CharField(max_length=200)
     altitude = models.CharField(max_length=20)
@@ -10,33 +17,21 @@ class Extra(models.Model):
     normal_bikes = models.BigIntegerField()
     payment = ArrayField(models.CharField(max_length=20))
     payment_terminal = models.BooleanField()
-    post_code = models.CharField(max_length=20)
     renting = models.BigIntegerField()
     returning = models.BigIntegerField() 
     slots = models.BigIntegerField()
-    uid = models.CharField(max_length=20)
 
 class Stations(models.Model):
-    id_station = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
     date = models.DateTimeField()
     latitude = models.CharField(max_length=40)
     longitude = models.CharField(max_length=40)
     free_bikes = models.BigIntegerField()
     empty_slots = models.BigIntegerField()
-    extra = models.OneToOneField(Extra, on_delete=models.CASCADE, primary_key = True)
-
-class Location(models.Model):
-    city = models.CharField(max_length=200)
-    country = models.CharField(max_length=200)
-    latitude = models.CharField(max_length=200)
-    longitude = models.CharField(max_length=200)
 
 class Network(models.Model):
     company = ArrayField(models.CharField(max_length=200))
     gbfs_href = models.CharField(max_length=200)
     href = models.CharField(max_length=200)
-    id_network = models.CharField(max_length=200)
-    location = models.OneToOneField(Location, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=200)
-    stations = models.ForeignKey(Stations, on_delete= models.CASCADE)
+    
--- Tarea_1_API_bike/bike_app/bike/views.py
+++ Tarea_1_API_bike/bike_app/bike/views.py
@@ -1,5 +1,26 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
+import requests
+from bike.models import *
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the bike index.")
+   return HttpResponse("Hello, world. You're at the bike index.")
+
+#def get_data(request):
+url = "http://api.citybik.es/v2/networks/bikesantiago"
+response = requests.get(url).json()
+
+for extra in response["network"]["stations"]:
+   Extra.objects.create(address=extra["extra"]["address"], altitude=extra["extra"]["altitude"], ebikes=extra["extra"]["ebikes"], has_bikes=extra["extra"]["has_ebikes"], last_updated=extra["extra"]["last_updated"], normal_bikes=extra["extra"]["normal_bikes"], payment=extra["extra"]["payment"], payment_terminal=extra["extra"]["payment-terminal"], renting=extra["extra"]["renting"], returning=extra["extra"]["returning"], slots=extra["extra"]["slots"])
+
+Location.objects.create(city=response["network"]["location"]["city"], country=response["network"]["location"]["country"],latitude=response["network"]["location"]["latitude"],longitude=response["network"]["location"]["longitude"])
+
+for station in response["network"]["stations"]:
+   Stations.objects.create( name=station["name"], date=station["timestamp"], latitude=station["latitude"], longitude=station["longitude"], free_bikes=station["free_bikes"], empty_slots=station["empty_slots"])
+
+Network.objects.create(company=response["network"]["company"],gbfs_href=response["network"]["gbfs_href"], href=response["network"]["href"],name=response["network"]["name"])
+   #return render(request, 'home.html', {'response':response})
+
+def station(request):
+   station = Stations.objects.all()
+   return render(request, "home.html", {'station':station})
